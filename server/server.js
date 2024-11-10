34ddb455bb7bf66d4b0044a2b245e6da6f5c6c4b
Navneet <navneet.72004@gmail.com>
2024-11-10 17:34:21 +0530

    fileupload frontend changes

--- server/server.js
+++ server/server.js
@@ -32,14 +32,5 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: 'Something went wrong!' });
 });
 
-// Add this near your other middleware
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-  
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-  });
-}
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
